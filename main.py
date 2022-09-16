5f6aa5ab040203aa9eea10957215d47f6c4eaf9f
Nathan Colburn <64048669+natecolburn@users.noreply.github.com>
2022-09-16 12:35:06 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+
+def guess(x):
+    random_number = random.randint(1 , x)
+    guess = 0
+    while guess != random_number:
+       guess = input(f'Enter a number between 1 and {x}: ')
+       if guess < random_number:
+           print('incorrect: Number is to low')
+       elif guess > random_number:
+           print('incorrect: Number is to high')
+    print(f'Correct: {x} was the correct answer')
+
+
+guess(10)
+
